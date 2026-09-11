e4edc58d277fd78f2429f4d239fca45d24566c2f
LeTanTru <letantru@gmail.com>
2026-09-11 22:04:34 +0700

    feat: add header, footer, and branding assets to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,17 @@
 import type { PropsWithChildren } from 'react'
+import Footer from '@/components/footer'
+import Header from '@/components/header'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='bg-linear-to-br from-background to-muted'>
       {/* Header */}
+      <Header />
 
       <main className='container mx-auto min-h-screen px-4 py-8'>{children}</main>
 
       {/* Footer */}
-      <footer className='border-t py-12 backdrop-blur supports-backdrop-filter:bg-background/60'>
-        <div className='container mx-auto px-4 text-center text-gray-400'>
-          <p>&copy; {new Date().getFullYear()} Le Tan Tru. All rights reserved.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
